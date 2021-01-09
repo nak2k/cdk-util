76d341de2b88901320c38500c0508b91599bb4cc
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2021-01-10 08:55:20 +0900

    Add NodejsFunction

---
+++ packages/@cdk-util/aws-lambda/src/NodejsFunction.ts
@@ -0,0 +1,68 @@
+import { Code, Function, FunctionBase, FunctionProps, Runtime } from '@aws-cdk/aws-lambda';
+import type { IPrincipal, IRole } from '@aws-cdk/aws-iam';
+import { Construct, ConstructNode } from '@aws-cdk/core';
+import { NodejsLayerVersion, NodejsLayerVersionProps } from './NodejsLayerVersion';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export interface NodejsFunctionProps extends Partial<FunctionProps>, Omit<NodejsLayerVersionProps, 'providerOnly'> {
+}
+
+export class NodejsFunction extends FunctionBase {
+  readonly handler: Function;
+  readonly nodejsLayerVersion: NodejsLayerVersion;
+
+  get layerVersion() {
+    return this.nodejsLayerVersion.layerVersion;
+  }
+
+  readonly grantPrincipal: IPrincipal;
+  readonly functionName: string;
+  readonly functionArn: string;
+  readonly role?: IRole;
+  readonly permissionsNode: ConstructNode;
+  protected readonly canCreatePermissions: boolean;
+
+  constructor(scope: Construct, id: string, props: NodejsFunctionProps) {
+    super(scope, id);
+
+    const {
+      packageDirectory,
+      useLockFile,
+      npmArgs,
+      code,
+      runtime,
+      handler,
+      layers,
+      ...restProps
+    } = props;
+
+    this.nodejsLayerVersion = new NodejsLayerVersion(this, 'LayerVersion', {
+      packageDirectory,
+      useLockFile,
+      npmArgs,
+    });
+
+    this.handler = new Function(this, 'Function', {
+      code: code || NodejsFunction.getCode(packageDirectory),
+      runtime: runtime || Runtime.NODEJS_12_X,
+      handler: handler || 'index.handler',
+      layers: layers ? [...layers, this.layerVersion] : [this.layerVersion],
+      ...restProps
+    });
+
+    this.grantPrincipal = this.handler.grantPrincipal;
+    this.functionName = this.handler.functionName;
+    this.functionArn = this.handler.functionArn;
+    this.role = this.handler.role;
+    this.permissionsNode = this.handler.permissionsNode;
+    this.canCreatePermissions = true;
+  }
+
+  private static getCode(packageDirectory: string) {
+    const pkg = readFileSync(join(packageDirectory, 'package.json'), 'utf8');
+    const { main } = JSON.parse(pkg);
+
+    return Code.fromAsset(main ? join(packageDirectory, main) : packageDirectory);
+  }
+}
--- packages/@cdk-util/aws-lambda/src/index.ts
+++ packages/@cdk-util/aws-lambda/src/index.ts
@@ -1,1 +1,2 @@
+export { NodejsFunction, NodejsFunctionProps } from './NodejsFunction';
 export { NodejsLayerVersion, NodejsLayerVersionProps } from './NodejsLayerVersion';
