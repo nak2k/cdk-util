e8f7ba473052f2745f3c9dcefa9611fac2e8c397
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2022-05-24 23:11:52 +0900

    Upgrade to AWS CDK v2

--- packages/@cdk-util/aws-iam/src/index.ts
+++ packages/@cdk-util/aws-iam/src/index.ts
@@ -1,6 +1,3 @@
-export { RoleModifier } from './RoleModifier';
-export { PermissionsBoundaryAspect } from './PermissionsBoundaryAspect';
 export { grantActions } from './grantActions';
 export { grantForSSMManagedInstance } from './grantForSSMManagedInstance';
 export { grantStack, grantStackByArn, grantStackByName, grantStacks } from './grantStack';
-export { grantSynth } from './grantSynth';
